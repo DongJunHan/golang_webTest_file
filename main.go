431069292b65e9f63a9eb491811aa75910a9ae5f
DongJunHan <gamedokdok@naver.com>
2021-02-18 23:35:34 +0900

    파일 업로드 예제 기본 완성

--- main.go
+++ main.go
@@ -2,9 +2,36 @@ package main
 
 import(
 	"net/http"
+	"os"
+	"io"
+	"fmt"
 )
 
+func uploadsHandler(w http.ResponseWriter, r *http.Request){
+	uploadFile, header, err := r.FormFile("upload_file")
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w,err)
+		return
+	}
+	defer uploadFile.Close()
+	dirname := "./uploads"
+	os.MkdirAll(dirname,0755)
+	filepath := fmt.Sprintf("%s/%s",dirname,header.Filename)
+	file, err1 := os.Create(filepath)
+	defer file.Close()
+	if err1 != nil{
+		w.WriteHeader(http.StatusInternalServerError)
+		fmt.Fprint(w,err)
+		return
+	}
+	io.Copy(file,uploadFile)
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w,filepath)
+}
+
 func main(){
+	http.HandleFunc("/uploads",uploadsHandler)
 	http.Handle("/",http.FileServer(http.Dir("public")))
 	
 	http.ListenAndServe(":3000",nil)
